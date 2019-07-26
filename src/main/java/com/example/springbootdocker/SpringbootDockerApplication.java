fdf6785665380274ed61376e8f9422a33dde6e7f
zhaoyunxing92 <2385585770@qq.com>
2019-07-26 11:52:18 +0800

    [up]添加测试代码gzip

--- src/main/java/com/example/springbootdocker/SpringbootDockerApplication.java
+++ src/main/java/com/example/springbootdocker/SpringbootDockerApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.naming.Name;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -17,8 +20,39 @@ public class SpringbootDockerApplication {
     }
 
     @GetMapping
-    public String say() {
-        return "hello docker" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss:SSS"));
+    public List<Per> say() {
+        List<Per> list = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            list.add(new Per(i, "sunny"));
+        }
+        return list;
     }
 
+    public class Per {
+        private Integer id;
+        private String name;
+
+        public Per(Integer id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        public Per setId(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Per setName(String name) {
+            this.name = name;
+            return this;
+        }
+    }
 }
